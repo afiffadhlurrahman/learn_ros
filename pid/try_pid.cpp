d90d14907c249e8d4b21d9f4fc36e1cf589c292c
Afif Fadhlurrahman <afif.fadhlurrahman1@gmail.com>
2020-09-01 20:55:19 +0700

    Update try_pid.cpp

--- pid/try_pid.cpp
+++ pid/try_pid.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class PID{
-    public:
+    private:
         double _dt;
         double _max;
         double _min;
@@ -11,8 +11,9 @@ class PID{
         double _Ki;
         double _pre_error;
         double _integral;
+    public:
         PID(double dt, double max, double min, double Kp, double Kd, double Ki);
-        //double calculate(double setpoint, double pv);
+        double calculate(double setpoint, double pv);
 };
 
 PID::PID(double dt, double max, double min, double Kp, double Kd, double Ki){
@@ -27,39 +28,46 @@ PID::PID(double dt, double max, double min, double Kp, double Kd, double Ki){
 
 }
 
-// double PID::calculate(double setpoint, double pv){
-//     // Calculate error
-//     double error = setpoint - pv;
+double PID::calculate(double setpoint, double pv){
+    // Calculate error
+    double error = setpoint - pv;
 
-//     // Proportional term
-//     double Pout = _Kp * error;
+    // Proportional term
+    double Pout = _Kp * error;
 
-//     // Integral term
-//     _integral += error * _dt;
-//     double Iout = _Ki * _integral;
+    // Integral term
+    _integral += error * _dt;
+    double Iout = _Ki * _integral;
 
-//     // Derivative term
-//     double derivative = (error - _pre_error) / _dt;
-//     double Dout = _Kd * derivative;
+    // Derivative term
+    double derivative = (error - _pre_error) / _dt;
+    double Dout = _Kd * derivative;
 
-//     // Calculate total output
-//     double output = Pout + Iout + Dout;
+    // Calculate total output
+    double output = Pout + Iout + Dout;
 
-//     // Restrict to max/min
-//     if( output > _max )
-//         output = _max;
-//     else if( output < _min )
-//         output = _min;
+    // Restrict to max/min
+    if( output > _max )
+        output = _max;
+    else if( output < _min )
+        output = _min;
 
-//     // Save error to previous error
-//     _pre_error = error;
+    // Save error to previous error
+    _pre_error = error;
 
-//     return output;
-// }
+    return output;
+}
 
 
 int main(){
     PID mypid(0.1, 100, -100, 0.1, 0.01, 0.5);
-    cout << mypid._pre_error << endl;
+    double val = 20;
+
+    for(int i=0; i<100;i++){
+        double inc = mypid.calculate(0, val);
+        printf("val:% 7.3f inc:% 7.3f\n", val, inc);
+        val += inc;
+    }
+    
     return 0;
 }
